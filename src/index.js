c33fc2d67e026e0ec65d354153bcb7edb1fd4cc6
Gambole <lucuanostrike@gmail.com>
2022-05-20 15:55:34 +0100

    adicionano um comentário ao index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react';               //importação dos pacotes
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
