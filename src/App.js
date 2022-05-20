515745e9adec5cb1cad65952dfd12bc51614a67d
Gambole <lucuanostrike@gmail.com>
2022-05-20 22:19:07 +0100

    adicionando componentes

--- src/App.js
+++ src/App.js
@@ -2,14 +2,115 @@ import logo from './logo.svg';
 import './App.css';
 
 
+//vamos criar um componente
+function MyButton() {
+  return (
+    <button>Clica me</button>
+  );
+}
+
+function AboutPage() {
+  return (
+    <>
+      <h1>About</h1>
+      <p>Hello there.<br />How do you do?</p>
+    </>
+  );
+}
+
+//usar IF
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+};
+
+function AdminPanel() {
+  return (
+    <p>Admin Panel</p>
+  );
+}
+
+function LoginForm() {
+  return (
+    <p>Login Form</p>
+  );
+}
+export default function Profile() {
+  let content;
+  let isLoggedIn = false;
+if (isLoggedIn) {
+  content = <AdminPanel/>;
+} else {
+  content = <LoginForm/>;
+}
+  return (
+    <>
+      {content}
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
+    </>
+  );
+}
+//OPERADOR TERNARIO
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+};
+
+function AdminPanel() {
+  return (
+    <p>Admin Panel</p>
+  );
+}
+
+function LoginForm() {
+  return (
+    <p>Login Form</p>
+  );
+}
+export default function Profile() {
+  let content;
+  let isLoggedIn = true;
+  return (
+    <>
+      {
+        isLoggedIn ? <AdminPanel/> : <LoginForm/>
+      }
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
+    </>
+  );
+}
+
+
+
 
 function App() {
   return (
     <div>
       <h1>Bem vindo a minha aplicação</h1>
+      <MyButton />
+      <AboutPage/>
     </div>
   );
 }
-
 
 export default App;
