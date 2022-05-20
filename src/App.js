55a43a7402e34225fe1141909b3f8271921d2a7c
Gambole <lucuanostrike@gmail.com>
2022-05-20 15:57:34 +0100

    Refazendo a App.js

--- src/App.js
+++ src/App.js
@@ -2,49 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 
-function MyButtom(){
-  return (
-    <button>Clica-me</button>
-  )
-
-}
-const user = {
-  name: 'Hedy Lamarr',
-  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
-  imageSize: 90,
-};
 
 function App() {
   return (
     <div>
       <h1>Bem vindo a minha aplicação</h1>
-      <MyButtom/>
-      <img className="avatar" />
-
-      <h1> {user.name} </h1>
-      <img className="avatar" src={user.imageUrl} />
-
-      <h1>{user.name}</h1>
-      <img
-        className="avatar"
-        src={user.imageUrl}
-        alt={'Photo of ' + user.name}
-        style={{
-          width: user.imageSize,
-          height: user.imageSize
-        }}
-      />
     </div>
   );
 }
 
-function Sobre() {
-  return (
-    <>
-      <h1>Sobre</h1>
-      <p>Olá<br />Como tu estás?</p>
-    </>
-  );
-}
 
 export default App;
